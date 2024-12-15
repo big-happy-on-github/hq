3898a8a04e9c6414a8371955a87ce34639155d81
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-12-15 10:44:20 -0500

    Update and rename hello.js to secure.js

---
+++ netlify/functions/secure.js
@@ -0,0 +1,7 @@
+exports.handler = async event => {
+    return {
+        statusCode: 200,
+        body: [{ "p1": "a", "p2": "b", "p3": "c", "q1": "d", "q2": "e", "q3": "f" }
+              ],
+    }
+}
